5d89db1ea3b48b900e66e6c1151a9ef254760376
Jaswanth Satya Dev <jaswanthjuluru55@gmail.com>
2025-08-13 07:47:01 +0000

    its a png not svg

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import type { SVGProps } from "react";
+import type { ComponentProps } from "react";
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: Omit<ComponentProps<typeof Image>, 'src' | 'alt'>) {
   // Using next/image to load the logo from the public folder.
   // The 'width' and 'height' are required props for next/image, 
   // but className can be used for responsive sizing.
   return (
     <Image 
-      src="/logo.svg" 
+      src="/logo.png" 
       alt="Evolv AI Agency Logo"
       width={134}
       height={40}
