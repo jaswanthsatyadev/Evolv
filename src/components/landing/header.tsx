7be410b21029128318b48ce93fa4edaa9cbc37c1
Jaswanth Satya Dev <jaswanthjuluru55@gmail.com>
2025-08-13 07:49:15 +0000

    nope not

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -20,7 +20,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-20 max-w-screen-2xl items-center justify-between px-4 md:px-6">
         <Link href="#" className="flex items-center gap-2 font-headline" prefetch={false}>
-          <Logo className="h-10 w-auto" />
+          <Logo className="h-12 w-auto" />
         </Link>
         <nav className="hidden md:flex items-center gap-8 text-sm font-medium">
           {navLinks.map((link) => (
